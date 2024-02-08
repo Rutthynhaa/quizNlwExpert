ce5df2032ef5d5b115eb172712fe2194e14a6bd9
Rutthynhaa <155782102+Rutthynhaa@users.noreply.github.com>
2024-02-08 00:15:55 -0300

    perguntasJogos

--- index.js
+++ index.js
@@ -1,133 +1,138 @@
 const perguntas = [
     {
-        pergunta: "Qual é a função que usamos para declarar uma variável em JavaScript?",
-        respostas: [
-            "define",
-            "var",
-            "declare"
-        ],
-        correta: 1 // "var"
+      pergunta: "Qual é o nome do encanador italiano que é o protagonista de uma famosa franquia de videogame da Nintendo?",
+      respostas: [
+        "Mario",
+        "Luigi",
+        "Yoshi",
+      ],
+      correta: 0
     },
     {
-        pergunta: "Qual destes métodos é usado para adicionar um elemento ao final de um array em JavaScript?",
-        respostas: [
-            "push()",
-            "add()",
-            "append()"
-        ],
-        correta: 0 // "push()"
+      pergunta: "Em que jogo é possível encontrar o personagem Sonic, o ouriço azul rápido, que é um dos mascotes mais conhecidos da SEGA?",
+      respostas: [
+        "The Legend of Zelda",
+        "Pokémon",
+        "Sonic the Hedgehog",
+      ],
+      correta: 2
     },
     {
-        pergunta: "Qual é a palavra-chave usada para declarar uma função em JavaScript?",
-        respostas: [
-            "function",
-            "def",
-            "method"
-        ],
-        correta: 0 // "function"
+      pergunta: "Qual é o nome do criador da série de jogos 'Metal Gear'?",
+      respostas: [
+        "Hideo Kojima",
+        "Shigeru Miyamoto",
+        "Satoru Iwata",
+      ],
+      correta: 0
     },
     {
-        pergunta: "Qual método é usado para selecionar um elemento pelo seu ID em JavaScript?",
-        respostas: [
-            "getElementByClass()",
-            "selectByID()",
-            "getElementById()"
-        ],
-        correta: 2 // "getElementById()"
+      pergunta: "Qual é o título do jogo onde os jogadores constroem e exploram um mundo virtual tridimensional?",
+      respostas: [
+        "Minecraft",
+        "World of Warcraft",
+        "League of Legends",
+      ],
+      correta: 0
     },
     {
-        pergunta: "Qual destas é uma maneira válida de comentar código em JavaScript?",
-        respostas: [
-            "// Este é um comentário",
-            "' Este é um comentário",
-            "<!-- Este é um comentário -->"
-        ],
-        correta: 0 // "// Este é um comentário"
+      pergunta: "Em que jogo é possível encontrar o personagem principal chamado Link, que deve resgatar a Princesa Zelda?",
+      respostas: [
+        "Final Fantasy",
+        "The Legend of Zelda",
+        "Halo",
+      ],
+      correta: 1
     },
     {
-        pergunta: "Qual método é usado para converter uma string em minúsculas em JavaScript?",
-        respostas: [
-            "toLowerCase()",
-            "toLower()",
-            "lowerCase()"
-        ],
-        correta: 0 // "toLowerCase()"
+      pergunta: "Qual é o nome do jogo onde os jogadores controlam um personagem chamado Nathan Drake em busca de tesouros perdidos?",
+      respostas: [
+        "Uncharted",
+        "Assassin's Creed",
+        "Gears of War",
+      ],
+      correta: 0
     },
     {
-        pergunta: "Qual operador é usado para verificar igualdade estrita em JavaScript?",
-        respostas: [
-            "==",
-            "===",
-            "="
-        ],
-        correta: 1 // "==="
+      pergunta: "Em que jogo os jogadores podem encontrar o famoso easter egg 'Konami Code'?",
+      respostas: [
+        "Mortal Kombat",
+        "Street Fighter",
+        "Contra",
+      ],
+      correta: 2
     },
     {
-        pergunta: "Qual é o resultado da expressão '5' + 2 em JavaScript?",
-        respostas: [
-            "7",
-            "52",
-            "TypeError"
-        ],
-        correta: 1 // "52"
+      pergunta: "Qual é o nome do jogo onde os jogadores assumem o papel de um herói chamado Geralt de Rivia em busca de sua filha adotiva?",
+      respostas: [
+        "The Witcher",
+        "Dark Souls",
+        "Bloodborne",
+      ],
+      correta: 0
     },
     {
-        pergunta: "Qual método é usado para remover o último elemento de um array em JavaScript?",
-        respostas: [
-            "pop()",
-            "removeLast()",
-            "deleteLast()"
-        ],
-        correta: 0 // "pop()"
+      pergunta: "Qual é o nome do estúdio de desenvolvimento responsável pela criação da série de jogos 'Grand Theft Auto'?",
+      respostas: [
+        "Ubisoft",
+        "Rockstar Games",
+        "Electronic Arts",
+      ],
+      correta: 1
     },
     {
-        pergunta: "Qual é a função usada para imprimir algo no console em JavaScript?",
-        respostas: [
-            "console.print()",
-            "log()",
-            "print()"
-        ],
-        correta: 1 // "log()"
+     
+      pergunta: "No universo dos jogos, qual desses é um dos tipos de criaturas que os jogadores podem capturar e treinar em 'Pokémon'?",
+      respostas: [
+        "Dragões",
+        "Zumbis",
+        "Monstros de bolso",
+      ],
+      correta: 2
+  
     }
-];
-
-
-
-const quiz = document.querySelector('#quiz')
-const template = document.querySelector('template')
-
-const corretas = new Set()
-const totalDePerguntas = perguntas.length
-const mostrarTotal = document.querySelector('#acertos span')
-mostrarTotal.textContent = corretas.size + 'de' + totalDePerguntas
-
-//loop ou laço de repetiçao
-for(const item of perguntas) {
-  const quizItem = template.content.cloneNode(true)
-  quizItem.querySelector('h3').textContent = item.pergunta
-
-  for(let resposta of item.respostas) {
-    const dt = quizItem.querySelector('dl dt').cloneNode(true)
-    dt.querySelector('span').textContent = resposta
-    dt.querySelector('input').setAttribute('name', 'pergunta-' + perguntas.indexOf(item))
-    dt.querySelector('input').value = item.respostas.indexOf(resposta)
-    dt.querySelector('input').onchange = (event) => {
-    const estaCorreta = event.target.value == item.correta
-    if(estaCorreta) {
-      corretas.add(item)
-     }
-    mostrarTotal.textContent = corretas.size + 'de' + totalDePerguntas
-
-    }
-
-    quizItem.querySelector('dl').appendChild(dt)
-  }
-  //espeço no parentese significa que dentro do dl está o dt 
-    quizItem.querySelector('dl dt').remove()
-
-  //aparece na tela
-quiz.appendChild(quizItem)
+  ];
   
   
-  }
+  
+  
+  
+  
+  const quiz = document.querySelector('#quiz')
+  const template = document.querySelector('template')
+  
+  const corretas = new Set()
+  const totalDePerguntas = perguntas.length
+  const mostrarTotal = document.querySelector('#acertos span')
+  mostrarTotal.textContent = corretas.size + 'de' + totalDePerguntas
+  
+  //loop ou laço de repetiçao
+  for(const item of perguntas) {
+    const quizItem = template.content.cloneNode(true)
+    quizItem.querySelector('h3').textContent = item.pergunta
+  
+    for(let resposta of item.respostas) {
+      const dt = quizItem.querySelector('dl dt').cloneNode(true)
+      dt.querySelector('span').textContent = resposta
+      dt.querySelector('input').setAttribute('name', 'pergunta-' + perguntas.indexOf(item))
+      dt.querySelector('input').value = item.respostas.indexOf(resposta)
+      dt.querySelector('input').onchange = (event) => {
+      const estaCorreta = event.target.value == item.correta
+      if(estaCorreta) {
+        corretas.add(item)
+       }
+      mostrarTotal.textContent = corretas.size + 'de' + totalDePerguntas
+  
+      }
+  
+      quizItem.querySelector('dl').appendChild(dt)
+    }
+    //espeço no parentese significa que dentro do dl está o dt 
+      quizItem.querySelector('dl dt').remove()
+  
+    //aparece na tela
+  quiz.appendChild(quizItem)
+    
+    
+    }
   
